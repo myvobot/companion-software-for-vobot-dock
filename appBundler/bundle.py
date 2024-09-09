a6f3381c274c1bfa45bfaa4bdac4f52b0f7e27fc
thomas <thomas@oazon.com>
2024-09-09 18:18:42 +0800

    fix(appBundler): Support parsing the latest manifest.yml file

--- appBundler/bundle.py
+++ appBundler/bundle.py
@@ -7,6 +7,7 @@ import platform
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import yaml
+import webbrowser
 
 def load_manifest(manifest_path):
     with open(manifest_path, 'r', encoding='utf-8') as file:
@@ -14,6 +15,7 @@ def load_manifest(manifest_path):
 
 def should_include(file_path, include_paths, exclude_paths):
     # Check if it is in the include list
+    if file_path.endswith("manifest.yml"): return True
     for include in include_paths:
         if file_path.startswith(include[:-3] if include.endswith(".py") else include):
             # Check if it is in the exclude list
@@ -31,9 +33,10 @@ def zip_folder(file_name, folder_path, output_path, include_paths=None, exclude_
             for file in files:
                 file_path = os.path.join(root, file)
                 arcname = os.path.join(parent_folder, os.path.relpath(file_path, folder_path))
-                print(file_path)
-                if include_paths is None or should_include(file_path, include_paths, exclude_paths):
+                if not include_paths or should_include(file_path, include_paths, exclude_paths):
+                    print("------save------")
                     zipf.write(file_path, arcname)
+                print(file_path)
 
 def get_mpy_cross_executable():
     if hasattr(sys, '_MEIPASS'):
@@ -98,6 +101,44 @@ def convert_py_to_mpy(src_folder, dst_folder, exclude_folder):
                 shutil.copy(file_path, output_dir)
     return True
 
+def open_link(event):
+    webbrowser.open_new(r"https://dock.myvobot.com/developer/guides/publishing-guide/manifest_file")
+
+def show_custom_messagebox(message_text):
+    custom_box = tk.Tk()
+    custom_box.title("Warning")
+
+    custom_box.geometry("300x150")
+    custom_box.resizable(False, False)
+
+    message = tk.Label(custom_box, text=message_text, wraplength=280)
+    message.pack(pady=5)
+
+    link = tk.Label(custom_box, text="Manifest.yml Requirements", fg="blue", cursor="hand2")
+    link.pack(pady=4)
+    link.bind("<Button-1>", open_link)
+
+    button = tk.Button(custom_box, text="OK", command=custom_box.destroy)
+    button.pack(pady=5)
+
+    custom_box.update_idletasks()
+    width = custom_box.winfo_width()
+    height = custom_box.winfo_height()
+    x = (custom_box.winfo_screenwidth() // 2) - (width // 2)
+    y = (custom_box.winfo_screenheight() // 2) - (height // 2)
+    custom_box.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+    custom_box.mainloop()
+
+def validate_manifest(manifest):
+    required_fields = [
+        'name', 'description', 'version',
+        'minimum_version', 'compatible_devices'
+    ]
+    for field in required_fields:
+        if field not in manifest['application'] and field not in manifest['system_requirements']:
+            return False, f"Missing required field: {field}.\nFor the content and required fields of manifest.yml, please visit: "
+    return True, ""
 
 def main():
     root = tk.Tk()
@@ -120,6 +161,17 @@ def main():
         return
 
     manifest_path = os.path.join(folder_path, 'manifest.yml')
+
+    if not os.path.exists(manifest_path):
+        show_custom_messagebox("Missing manifest.yml.\nFor the content and required fields of manifest.yml, please visit: ")
+        return
+
+    manifest = load_manifest(manifest_path)
+    is_valid, error_message = validate_manifest(manifest)
+    if not is_valid:
+        show_custom_messagebox(error_message)
+        return
+
     include_paths = exclude_paths = None
 
     open_source = messagebox.askyesno("Open Source", "Allow code to be open source?")
@@ -131,11 +183,13 @@ def main():
             shutil.rmtree(temp_folder)  # Clean up temporary folder
             return
         folder_path = temp_folder
-
-    if os.path.exists(manifest_path):
-        manifest = load_manifest(manifest_path).get("manifest",{})
-        include_paths = [os.path.normpath(os.path.join(folder_path, item['path'])) for item in manifest.get('include', [])]
-        exclude_paths = [os.path.normpath(os.path.join(folder_path, item['path'])) for item in manifest.get('exclude', [])]
+    try:
+        if 'files' in manifest and 'include' in manifest['files']:
+            include_paths = [os.path.normpath(os.path.join(folder_path, item)) for item in manifest['files'].get('include', [])]
+            exclude_paths = [os.path.normpath(os.path.join(folder_path, item)) for item in manifest['files'].get('exclude', [])]
+    except:
+        include_paths = None
+        exclude_paths = None
 
     output_path = filedialog.asksaveasfilename(defaultextension=".vbt", filetypes=[("VBT files", "*.vbt")], title="Save bundled file", initialfile=file_name + ".vbt")
     if not output_path:
